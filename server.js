272c8fa2c08b1ee304416e9120906117a7230353
lizazav <liziktele2@gmail.com>
2022-01-09 17:52:51 +0700

    heroku

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('src/backend/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
-
 //Install express server
 const express = require('express');
 const path = require('path');
